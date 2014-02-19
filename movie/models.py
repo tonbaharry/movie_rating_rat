6e91caa48bd7d729e7832b8aa1ef8514cda4429f
x <pawel@fibrotrust.org>
2014-02-19 21:14:17 +0000

    initial commit

---
+++ movie/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+	
+	def __unicode__(self):
+		return self.name
+
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+	
+	def __unicode__(self):
+		return self.title
+
+class UserProfile(models.Model):
+	# A required line - links a UserProfile to User.
+	user = models.OneToOneField(User)
+	
+	# The additional attributes we wish to include.
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', default='/home/x/code/movie_rating_project/media/profile_images/sample_profile.jpg')
+
+	def __unicode__(self):
+		return self.user.username
+
+class User(models.Model):
+    name = models.EmailField(max_length= 128,primary_key=True)
+    password = models.CharField(max_length=20)
+    dob = models.DateTimeField(auto_now= False, auto_now_add= False)
+
+    def __unicode__(self):
+        return self.name
+
+
+#------------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------
+class Category(models.Model):
+    catName = models.CharField(max_length=30, unique=True)
+
+
+class Movie(models.Model):
+    cat = models.ForeignKey(Category)
+    movieName = models.CharField(max_length=128)
+    releaseYear = models.DateTimeField()
+    coverPhoto = models.ImageField(upload_to='movie_images', default='media/profile_images/rango.jpg')
+    trailer = models.URLField()
+
+    class meta:
+        together = ("movieName", "releaseYear")
+
+class Ratings(models.Model):
+    name = models.ForeignKey(User)
+    movieName = models.ForeignKey(Movie)
+    rates = models.IntegerField(max_length=5)
+
+
+class Comments(models.Model):
+    name = models.ForeignKey(User)
+    MovieName = models.ForeignKey(Movie)
+    comment = models.CharField(max_length=500, null=False)
+    commentDate = models.DateField()
---
+++ movie_rating_project/urls.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'movie_rating_project.views.home', name='home'),
+    # url(r'^movie_rating_project/', include('movie_rating_project.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^movie/', include('movie.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+	urlpatterns += patterns(
+		'django.views.static',
+		(r'media/(?P<path>.*)',
+		'serve',
+		{'document_root': settings.MEDIA_ROOT}),)
